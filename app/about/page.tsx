e8a030343e11fd7add489e72485a1bd3fd9fd372
NadraAmriii <nadraamri568@gmail.com>
2025-09-12 12:34:29 +0100

    first commit

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+      );
+}
+ 
+export default About;
